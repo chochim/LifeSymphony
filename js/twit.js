257c1a0631d3e0253ab56c6801f3224245c37cd0
Rohit Prakash <rohitprakash@gmail.com>
2017-03-31 19:17:38 -0400

    twitter tweets searched

--- js/twit.js
+++ js/twit.js
@@ -3,74 +3,76 @@ $(document).ready(function() {
 });
 
 function hideLoader() {
-	$('#loader').hide();
+    $('#loader').hide();
 }
 
 function showLoader() {
-	$('#loader').show();
+    $('#loader').show();
 }
 
 function getSingleTweetDisplay(handle, tweet) {
-	var tweetDisplay = '<li class="collection-item avatar">';
-	tweetDisplay += '<img src="'+getImageUrl(handle)+'" alt="" class="circle">';
-	tweetDisplay += '<span class="title">'+handle+'</span><p>'+tweet+'</p>';
-	tweetDisplay += '<a href="#!" class="secondary-content"><i class="material-icons">grade</i></a></li>';
-	return tweetDisplay;
+    var tweetDisplay = '<li class="collection-item avatar">';
+    tweetDisplay += '<img src="' + getImageUrl(handle) + '" alt="" class="circle">';
+    tweetDisplay += '<span class="title">' + handle + '</span><blockquote><i>' + tweet + '</i></blockquote>';
+    tweetDisplay += '<a href="#!" class="secondary-content"><i class="medium material-icons">play_arrow</i></a></li>';
+    return tweetDisplay;
 }
 
 function getDisplayResults(twitterObjs) {
-	var htmlString = '';
-	$.each(twitterObjs, function(handle, tweet){
-		htmlString += getSingleTweetDisplay(handle, tweet);
-	});
-	return htmlString;	
+    var htmlString = '';
+    $.each(twitterObjs, function(handle, tweet) {
+        htmlString += getSingleTweetDisplay(handle, tweet);
+    });
+    return htmlString;
 }
 
 function displayResults(twitterObjs) {
-	var htmlString  = getDisplayResults(twitterObjs);	
-	console.log(htmlString);
-	$('#collection').append(htmlString);
-	$('#collection').show();
+    var htmlString = getDisplayResults(twitterObjs);
+    $('.res').show();
+    $('.collection').append(htmlString);
+    $('.collection').show();
 }
 
 function getImageUrl(screenName) {
-	return 'https://twitter.com/'+screenName+'/profile_image?size=bigger';
+    return 'https://twitter.com/' + screenName + '/profile_image?size=bigger';
 }
 
 function showError(errorMsg) {
-	Materialize.toast(errorMsg, 4000);
+    Materialize.toast(errorMsg, 4000);
 }
 
 function removeList() {
-	$('ul li').not('li:first').remove()
+    $('ul li').not('li:first').remove()
 }
 
 function getTwitterResults(searchTerm) {
     $.post("twitter.php", { 'search_term': searchTerm }, function(data) {
-    	data = jQuery.parseJSON(data);
-    		if(data['error']) {    			
-    			showError(data['error']);
-    		} else {//if error
-    			//alert(JSON.stringify(data['result']));
-    			displayResults(data['result']);
-    		}            
+            data = jQuery.parseJSON(data);
+            if (data['error']) {
+                showError(data['error']);
+            } else { //if error
+                //alert(JSON.stringify(data['result']));
+                displayResults(data['result']);
+            }
         })
         .fail(function() {
-            showError('Error connecting to the server!');            
+            showError('Error connecting to the server!');
         })
         .always(function() {
-        	hideLoader();
-        });        
+            hideLoader();
+        });
 }
 
 (function() {
     var search = document.getElementById('search');
     search.addEventListener('keypress', function(event) {
         if (event.keyCode == 13) {
-            $('#loader').show();
-            event.preventDefault();  
-            removeList();          
-            getTwitterResults($('#search').val().trim());
+            if ($('#search').val().trim()) {
+                $('#loader').show();
+                event.preventDefault();
+                removeList();
+                getTwitterResults($('#search').val().trim());
+            }
         }
     });
 }());
